7c0df85dca6cd6e9ea2df657579bb7a093a6d8e1
Ono Masashi <masashi.ono@linecorp.com>
2015-01-14 17:13:51 +0900

    Updated Podfile settings. Added Quick testcases.

--- Podfile
+++ Podfile
@@ -1,5 +1,11 @@
 source 'https://github.com/CocoaPods/Specs.git'
 platform :ios, '8.0'
 
-pod 'Mantle', '~> 1.5'
-pod 'couchbase-lite-ios', '~> 1.0'
+target "ReactiveCocoaSwift" do
+  pod 'Mantle', '~> 1.5'
+  pod 'couchbase-lite-ios', '~> 1.0'
+end
+
+target "ReactiveCocoaSwiftTests" do
+
+end
---
+++ ReactiveCocoaSwiftTests/QuickNimbleTests.swift
@@ -0,0 +1,45 @@
+//
+//  QuickNimbleTests.swift
+//  ReactiveCocoaSwift
+//
+//  Created by Ono Masashi on 2015/01/14.
+//  Copyright (c) 2015年 akisute. All rights reserved.
+//
+
+import Foundation
+import Quick
+import Nimble
+
+class QuickNimbleTests: QuickSpec {
+    override func spec() {
+        var date: NSDate?
+        beforeEach {
+            date = NSDate()
+        }
+        afterEach {
+            date = nil
+        }
+        
+        it("passes some easiest tests") {
+            expect(true).to(beTruthy())
+            expect(3).to(equal(3))
+            expect("test").to(equal("test"))
+            expect(date != nil).to(beTruthy())
+            //expect(nil).to(beTruthy()) // DOES NOT WORK
+        }
+        
+        context("when there is an additional date") {
+            var date2: NSDate?
+            beforeEach {
+                date2 = NSDate()
+            }
+            afterEach {
+                date2 = nil
+            }
+            it("is obvious that date2 is later than date1") {
+                expect(date2 != nil).to(beTruthy())
+                expect(date!.laterDate(date2!)).to(equal(date2!))
+            }
+        }
+    }
+}
