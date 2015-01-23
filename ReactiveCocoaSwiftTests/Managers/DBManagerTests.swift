31d5c594237f9f6e9d5e4e39b02fd1575648cd0d
Ono Masashi <masashi.ono@linecorp.com>
2015-01-23 17:10:28 +0900

    Added several basic test cases fro DBManager

--- ReactiveCocoaSwiftTests/Managers/DBManagerTests.swift
+++ ReactiveCocoaSwiftTests/Managers/DBManagerTests.swift
@@ -34,7 +34,7 @@ class DBManagerTests: QuickSpec {
                     let document = manager.database.createDocument()
                     expect(document).toNot(beNil())
                     document.putProperties(["name": "akisute", "message": "Hello, World!"], error: nil)
-                    let document2 = manager.database.documentWithID(document.documentID)
+                    let document2 = manager.database.existingDocumentWithID(document.documentID)
                     expect(document2).toNot(beNil())
                     expect(document2.properties["name"] as String?).to(equal("akisute"))
                     expect(document2.properties["message"] as String?).to(equal("Hello, World!"))
@@ -48,11 +48,45 @@ class DBManagerTests: QuickSpec {
                     let document = manager.database.createDocument()
                     document.putProperties(["name": "akisute", "message": "Hello, World!"], error: nil)
                 }
-                it("can update existing document") {
-                    
+                it("can find existing document using View and Query") {
+                    let view = manager.database.viewNamed("name_to_message")
+                    view.setMapBlock({ (document, emit) -> Void in
+                        emit(document["name"], document["message"])
+                    }, version: "1")
+                    let query = view.createQuery()
+                    let result = query.run(nil)
+                    expect(result).toNot(beNil())
+                    expect(result!.count).to(equal(1))
+                    let row = result.nextRow()
+                    expect(row.document).toNot(beNil())
+                    expect(row.key as? String).to(equal("akisute"))
+                    expect(row.value as? String).to(equal("Hello, World!"))
                 }
-                it("can delete existing document") {
-                    
+                context("when document is found") {
+                    var document: CBLDocument!
+                    beforeEach {
+                        let view = manager.database.viewNamed("name_to_message")
+                        view.setMapBlock({ (document, emit) -> Void in
+                            emit(document["name"], document["message"])
+                            }, version: "1")
+                        let query = view.createQuery()
+                        let result = query.run(nil)
+                        document = result.nextRow().document
+                    }
+                    it("can update existing document") {
+                        var properties = document.properties
+                        properties["message"] = "Message Updated!"
+                        document.putProperties(properties, error: nil)
+                        let document2 = manager.database.existingDocumentWithID(document.documentID)
+                        expect(document2).toNot(beNil())
+                        expect(document2.properties["name"] as String?).to(equal("akisute"))
+                        expect(document2.properties["message"] as String?).to(equal("Message Updated!"))
+                    }
+                    it("can delete existing document") {
+                        document.deleteDocument(nil)
+                        let document2 = manager.database.existingDocumentWithID(document.documentID)
+                        expect(document2).to(beNil())
+                    }
                 }
             }
         }
